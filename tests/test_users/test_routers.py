b450ce08ff4fd9c8aab4547e23eba0ad64aac671
michal999 <71217112+mmyszak999@users.noreply.github.com>
2024-05-19 11:48:25 +0200

    refactor tests to run lazy fixtures in parametrize tests

--- tests/test_users/test_routers.py
+++ tests/test_users/test_routers.py
@@ -6,11 +6,14 @@ from httpx import AsyncClient, Response
 
 from src.apps.users.schemas import UserLoginInputSchema, UserOutputSchema
 from src.core.factories import UserInputSchemaFactory
-from tests.test_users.conftest import DB_USER_SCHEMA, PASSWORD_SCHEMA
+from tests.test_users.conftest import (
+    DB_USER_SCHEMA, PASSWORD_SCHEMA, db_staff_user, db_user,
+    auth_headers, staff_auth_headers
+)
 
 
 @pytest.mark.asyncio
-async def test_if_user_was_created_successfully(
+async def test_staff_can_create_new_user_account(
     async_client: AsyncClient,
     staff_auth_headers: dict[str, str],
     db_staff_user: UserOutputSchema
@@ -23,7 +26,34 @@ async def test_if_user_was_created_successfully(
     )
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["email"] == user_input_data.email
-    
+
+
+@pytest.mark.asyncio
+async def test_authenticated_user_cannot_create_new_user_account(
+    async_client: AsyncClient,
+    auth_headers: dict[str, str],
+    db_user: UserOutputSchema
+):
+    user_input_data = UserInputSchemaFactory().generate()
+    response = await async_client.post(
+        "users/create",
+        content=user_input_data.json(),
+        headers=auth_headers
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+async def test_anonymous_user_cannot_create_new_user_account(
+    async_client: AsyncClient,
+):
+    user_input_data = UserInputSchemaFactory().generate()
+    response = await async_client.post(
+        "users/create",
+        content=user_input_data.json()
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+ 
     
 @pytest.mark.asyncio
 async def test_if_user_was_logged_correctly(
@@ -36,3 +66,22 @@ async def test_if_user_was_logged_correctly(
     response = await async_client.post("users/login", content=login_data.json())
     assert response.status_code == status.HTTP_200_OK
     assert "access_token" in response.json()
+
+
+
+@pytest.mark.parametrize(
+    "user, user_headers",
+    [
+        (pytest.lazy_fixture('db_user'), pytest.lazy_fixture('auth_headers')),
+        (pytest.lazy_fixture('db_staff_user'), pytest.lazy_fixture('staff_auth_headers')),
+    ],
+)
+@pytest.mark.asyncio
+async def test_staff_and_authenticated_user_can_get_active_users(
+    async_client: AsyncClient, user: UserOutputSchema, user_headers: dict[str, str],
+    request: pytest.FixtureRequest
+):
+    response = await async_client.get("users/", headers=user_headers)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["total"] == 2
