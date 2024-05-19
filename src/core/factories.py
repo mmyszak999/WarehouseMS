fa200782d8893e8423be6d16694ee37bca344a79
michal999 <71217112+mmyszak999@users.noreply.github.com>
2024-05-19 14:50:29 +0200

    add user routers tests

--- src/core/factories.py
+++ src/core/factories.py
@@ -3,7 +3,7 @@ from datetime import date
 from decimal import Decimal
 from typing import Optional
 
-from src.apps.users.schemas import UserInputSchema, UserPasswordSchema
+from src.apps.users.schemas import UserInputSchema, UserPasswordSchema, UserUpdateSchema
 from src.core.utils.time import set_employment_date_for_factory
 from src.core.utils.faker import initialize_faker
 
@@ -59,4 +59,28 @@ class UserPasswordSchemaFactory(SchemaFactory):
         return self.schema_class(
             password=password,
             password_repeat=password_repeat
+        )
+
+class UserUpdateSchemaFactory(SchemaFactory):
+    def __init__(self, schema_class=UserUpdateSchema):
+        super().__init__(schema_class)
+
+    def generate(
+        self,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+        employment_date: Optional[date] = None,
+        birth_date: Optional[date] = None,
+        can_move_goods: Optional[bool] = None,
+        can_recept_goods: Optional[bool] = None,
+        can_issue_goods: Optional[bool] = None
+    ):
+        return self.schema_class(
+            first_name=first_name,
+            last_name=last_name,
+            employment_date=employment_date,
+            birth_date=birth_date,
+            can_move_goods=can_move_goods,
+            can_recept_goods=can_recept_goods,
+            can_issue_goods=can_issue_goods
         )
