14f9fc6b6bf649beba894b22f080deade5dc09a7
michal999 <71217112+mmyszak999@users.noreply.github.com>
2024-06-04 17:12:21 +0200

    create waiting rooms schemas factory

---
+++ src/core/factory/waiting_room_factory.py
@@ -0,0 +1,37 @@
+from datetime import date
+from decimal import Decimal
+from typing import Optional
+
+from src.apps.waiting_rooms.schemas import WaitingRoomInputSchema, WaitingRoomUpdateSchema
+from src.core.factory.core import SchemaFactory
+from src.core.utils.faker import set_waiting_room_weight, set_waiting_room_stocks
+
+
+class WaitingRoomInputSchemaFactory(SchemaFactory):
+    def __init__(self, schema_class=WaitingRoomInputSchema):
+        super().__init__(schema_class)
+
+    def generate(
+        self,
+        max_stocks: int = None,
+        max_weight: Decimal = None
+    ):
+        return self.schema_class(
+            max_stocks=max_stocks or set_waiting_room_stocks,
+            max_weight=max_weight or set_waiting_room_weight
+        )
+
+
+class WaitingRoomUpdateSchemaFactory(SchemaFactory):
+    def __init__(self, schema_class=WaitingRoomUpdateSchema):
+        super().__init__(schema_class)
+
+    def generate(
+        self,
+        max_stocks: Optional[int] = None,
+        max_weight: Optional[Decimal] = None
+    ):
+        return self.schema_class(
+            max_stocks=max_stocks,
+            max_weight=max_weight
+        )
